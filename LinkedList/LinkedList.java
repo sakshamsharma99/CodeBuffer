2f584f24ac768d22896e7f6efbc84e3561629eb9
Saksham Sharma <122073643+sakshamsharma99@users.noreply.github.com>
2025-01-27 00:30:31 +0530

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -34,12 +34,50 @@ public class LinkedList {
 			size++;
 		}
 	}
+	
+	public void addatindex(int item, int k) throws Exception {
+		if( k == 0 ) {
+			addfirst(item);
+		}else if( k == size ) {
+			addlast(item);
+		}else {
+			Node nn = new Node();
+			nn.val = item;
+			Node k_1th = GetNode(k-1);
+			nn.next = k_1th.next;	
+			k_1th.next = nn;
+		}
+	}
+	
+	private Node GetNode(int k) throws Exception {
+		if( k<0 || k >= size ) {
+			throw new Exception("Bklol k out of range hai");
+		}
+		Node temp = head;
+		Node nn = new Node();
+		for(int i=0; i < k; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
 	public void display() {
 		Node temp = head;
 		while (temp != null) {
 		System.out.print(temp.val + "->");	
 		temp = temp.next;
 		}
-		System.out.print(".");
+		System.out.println(".");
+	}
+	
+	public int getfirst() {
+		return head.val;
+	}
+	
+	public int getlast() {
+		return tail.val;
+	}
+	
+	public int getatindex( int k ) throws Exception {
+		return GetNode(k).val;
 	}
 }
